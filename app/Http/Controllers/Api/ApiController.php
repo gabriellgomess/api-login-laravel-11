b7bccad7e94fa0eae326e7f5b4ef8867dcf267c4
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2024-10-04 09:50:13 -0300

    implementada documentação Swagger

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
@@ -9,8 +11,66 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API de Autenticação",
+ *      description="Documentação da API de autenticação",
+ *      @OA\Contact(
+ *          email="gabriel.gomes@outlook.com"
+ *      )
+ *      
+ * )
+ */
+
 class ApiController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     summary="Registro de Usuário",
+     *     description="Registra um novo usuário.",
+     *     tags={"Autenticação"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name", "email", "password"},
+     *             @OA\Property(property="name", type="string", example="Nome Usuário"),
+     *             @OA\Property(property="email", type="string", format="email", example="usuario@email.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="senhaSegura123")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Usuário criado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="message", type="string", example="Usuário criado com sucesso"),
+     *             @OA\Property(property="token", type="string", example="1|V0upjdioPsDPjWdOyNGjJIaCQHJTJH0MQvwK5DdZ13806f99"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="name", type="string", example="Nome Usuário"),
+     *                 @OA\Property(property="email", type="string", example="usuario@email.com"),
+     *                 @OA\Property(property="created_at", type="string", example="2024-10-03T15:51:05.000000Z"),
+     *                 @OA\Property(property="updated_at", type="string", example="2024-10-03T15:51:05.000000Z"),
+     *                 @OA\Property(property="id", type="integer", example=1)
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Erro de validação",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Erro de Validação"),
+     *             @OA\Property(property="erros", type="object",
+     *                 @OA\Property(property="email", type="array", @OA\Items(type="string", example="Este email já foi cadastrado"))
+     *             )
+     *         )
+     *     )
+     * )
+     */
+
+
     public function register(Request $request)
     {
         try {
@@ -48,6 +108,50 @@ class ApiController extends Controller
             ], 500);
         }
     }
+
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="Login de Usuário",
+     *     description="Autentica um usuário.",
+     *     tags={"Autenticação"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"email", "password"},
+     *             @OA\Property(property="email", type="string", format="email", example="usuario@email.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="senhaSegura123")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Usuário logado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="message", type="string", example="Usuário logado com sucesso"),
+     *             @OA\Property(property="token", type="string", example="2|B6CL7DLBQHLFvpBPv4qHn2swyqeRH6lSAZOHsGEs66100ee8"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer", example=1),
+     *                 @OA\Property(property="name", type="string", example="Nome Usuário"),
+     *                 @OA\Property(property="email", type="string", example="usuario@email.com"),
+     *                 @OA\Property(property="email_verified_at", type="string", nullable=true, example=null),
+     *                 @OA\Property(property="created_at", type="string", example="2024-10-03T15:51:05.000000Z"),
+     *                 @OA\Property(property="updated_at", type="string", example="2024-10-03T15:51:05.000000Z")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Email ou senha incorretos",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Email ou senha incorretos")
+     *         )
+     *     )
+     * )
+     */
+
+
 
     public function login(Request $request)
     {
@@ -89,6 +193,42 @@ class ApiController extends Controller
         }
     }
 
+
+
+    /**
+     * @OA\Get(
+     *     path="/api/profile",
+     *     summary="Obter perfil do usuário autenticado",
+     *     description="Retorna o perfil do usuário autenticado.",
+     *     tags={"Autenticação"},
+     *     security={{ "sanctum": {} }},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Perfil do usuário recuperado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="message", type="string", example="User profile"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer", example=1),
+     *                 @OA\Property(property="name", type="string", example="Nome Usuário"),
+     *                 @OA\Property(property="email", type="string", example="usuario@email.com"),
+     *                 @OA\Property(property="email_verified_at", type="string", nullable=true, example=null),
+     *                 @OA\Property(property="created_at", type="string", example="2024-10-03T15:51:05.000000Z"),
+     *                 @OA\Property(property="updated_at", type="string", example="2024-10-03T15:51:05.000000Z")
+     *             ),
+     *             @OA\Property(property="id", type="integer", example=1)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Usuário não autenticado",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Usuário não autenticado.")
+     *         )
+     *     )
+     * )
+     */
+
     public function profile()
     {
         $userData = auth()->user();
@@ -99,6 +239,31 @@ class ApiController extends Controller
             'id' => $userData->id
         ], 200);
     }
+
+    /**
+     * @OA\Post(
+     *     path="/api/logout",
+     *     summary="Logout do usuário autenticado",
+     *     description="Efetua o logout do usuário autenticado.",
+     *     tags={"Autenticação"},
+     *     security={{ "sanctum": {} }},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Logout realizado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="message", type="string", example="Logout realizado com sucesso")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Usuário não autenticado",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Usuário não autenticado")
+     *         )
+     *     )
+     * )
+     */
 
     public function logout()
     {
